59b291f32d4c0da7639594b2dbd140bc6ff8c940
Slemmie <thorvejeneriksen@gmail.com>
2022-02-02 15:21:14 +0100

    create core.h + sld.h

---
+++ sld/sld.h
@@ -0,0 +1,18 @@
+// --------------------------------
+//  contains everything inside sld
+// --------------------------------
+
+#pragma once
+
+#ifndef _SLD_SLD_H
+#define _SLD_SLD_H
+
+// --------------------------------
+// begin sld headers
+
+#include <sld/core.h>
+
+// end sld headers
+// --------------------------------
+
+#endif /// _SLD_SLD_H
