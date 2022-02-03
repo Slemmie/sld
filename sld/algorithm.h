d5b80a802c75d2b6d4017647591ea7a31aa6345d
Slemmie <thorvejeneriksen@gmail.com>
2022-02-03 14:49:52 +0100

    create algorithm.h

---
+++ sld/algorithm.h
@@ -0,0 +1,37 @@
+// ----------------------------------
+//  defines various basic algorithms
+// ----------------------------------
+
+#pragma once
+
+#ifndef _SLD_ALGORITHM_H
+#define _SLD_ALGORITHM_H
+
+#include <sld/core.h>
+
+_SLD_BEGIN
+
+// begin min/max functions
+// custom comparators should return whether arg1 < arg2, regardless of use of min()/max()
+
+template <class A> const A& min(const A& v1, const A& v2) {
+	return v2 < v1 ? v2 : v1;
+}
+
+template <class A, class CMP> const A& min(const A& v1, const A& v2, const CMP& cmp) {
+	return cmp(v2, v1) ? v2 : v1;
+}
+
+template <class A> const A& max(const A& v1, const A& v2) {
+	return v1 < v2 ? v2 : v1;
+}
+
+template <class A, class CMP> const A& max(const A& v1, const A& v2, const CMP& cmp) {
+	return cmp(v1, v2) ? v2 : v1;
+}
+
+// end min/max functions
+
+_SLD_END
+
+#endif /// _SLD_ALGORITHM_H
