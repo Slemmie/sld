c82749929bbcf6c17ca474ce0f26f9a9482aedc4
Slemmie <thorvejeneriksen@gmail.com>
2022-02-03 15:12:03 +0100

    put base exception class outside of 'except' namespace

--- sld/exception.h
+++ sld/exception.h
@@ -11,38 +11,38 @@
 
 _SLD_BEGIN
 
+class exception {
+	
+public:
+	
+	const char* what() const noexcept {
+		return this->m_what;
+	}
+	
+protected:
+	
+	exception() = delete;
+	
+	exception(const char* _message) noexcept :
+	m_what(_message)
+	{ }
+	
+	const char* m_what;
+	
+};
+
 namespace except {
 	
-	class exception {
-		
-	public:
-		
-		const char* what() const noexcept {
-			return this->m_what;
-		}
-		
-	protected:
-		
-		exception() = delete;
-		
-		exception(const char* _message) noexcept :
-		m_what(_message)
-		{ }
-		
-		const char* m_what;
-		
-	};
-	
-	class out_of_bounds : public _SLD except::exception {
+	class out_of_bounds : public _SLD exception {
 		
 	public:
 		
 		out_of_bounds() noexcept :
-		_SLD except::exception("index out of range")
+		_SLD exception("index out of range")
 		{ }
 		
 		out_of_bounds(const char* _message) noexcept :
-		_SLD except::exception(_message)
+		_SLD exception(_message)
 		{ }
 		
 	};
