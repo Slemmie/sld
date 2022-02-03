75e203fd3bc22c7e0dc63ec4091e51dcfab03881
Slemmie <thorvejeneriksen@gmail.com>
2022-02-03 17:51:23 +0100

    make min/max functions inline

--- sld/algorithm.h
+++ sld/algorithm.h
@@ -14,19 +14,19 @@ _SLD_BEGIN
 // begin min/max functions
 // custom comparators should return whether arg1 < arg2, regardless of use of min()/max()
 
-template <class A> const A& min(const A& v1, const A& v2) {
+template <class A> inline const A& min(const A& v1, const A& v2) {
 	return v2 < v1 ? v2 : v1;
 }
 
-template <class A, class CMP> const A& min(const A& v1, const A& v2, const CMP& cmp) {
+template <class A, class CMP> inline const A& min(const A& v1, const A& v2, const CMP& cmp) {
 	return cmp(v2, v1) ? v2 : v1;
 }
 
-template <class A> const A& max(const A& v1, const A& v2) {
+template <class A> inline const A& max(const A& v1, const A& v2) {
 	return v1 < v2 ? v2 : v1;
 }
 
-template <class A, class CMP> const A& max(const A& v1, const A& v2, const CMP& cmp) {
+template <class A, class CMP> inline const A& max(const A& v1, const A& v2, const CMP& cmp) {
 	return cmp(v1, v2) ? v2 : v1;
 }
 
